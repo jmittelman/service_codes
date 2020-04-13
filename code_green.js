a7847746055c31be6d084bfe3e7a83dde0627ea5
Jordan <jordanmittelman@gmail.com>
2020-04-13 03:52:26 -0400

    complete the question tree structure

--- code_green.js
+++ code_green.js
@@ -1,10 +1,90 @@
 function externalDetailsReveal() {
   var checkBox = document.getElementById("source2");
   var guestDetails = document.getElementById("externalDetails");
+  var experienceContent = document.getElementById("allExperienceContent");
+  var recognitionContent = document.getElementById("recognition");
+  var followUpContent = document.getElementById("followUp");
   if (checkBox.checked == true) {
     guestDetails.style.display = "block";
+    experienceContent.style.display = "block";
+    recognitionContent.style.display = "block";
+    followUpContent.style.display = "block";
   } else {
     guestDetails.style.display = "none";
+    experienceContent.style.display = "block";
+    recognitionContent.style.display = "none";
+    followUpContent.style.display = "none";
+  }
+}
+
+function internalDetailsReveal() {
+  var checkBox = document.getElementById("source1");
+  var employeePraise = document.getElementById("allEmployeeContent");
+  var recognitionContent = document.getElementById("recognition");
+  var followUpContent = document.getElementById("followUp");
+  if (checkBox.checked == true) {
+    employeePraise.style.display = "block";
+    recognitionContent.style.display = "block";
+    followUpContent.style.display = "block";
+  } else {
+    employeePraise.style.display = "none";
+    recognitionContent.style.display = "none";
+    followUpContent.style.display = "none";
+  }
+}
+
+function internalDetailsReveal1() {
+  var checkBox = document.getElementById("source3");
+  var guestPraise = document.getElementById("guestPerspectiveContent");
+  var recognitionContent = document.getElementById("recognition");
+  var followUpContent = document.getElementById("followUp");
+  if (checkBox.checked == true) {
+    guestPraise.style.display = "block";
+    recognitionContent.style.display = "block";
+    followUpContent.style.display = "block";
+  } else {
+    guestPraise.style.display = "none";
+    recognitionContent.style.display = "none";
+    followUpContent.style.display = "none";
+  }
+}
+
+function externalDetailsReveal2() {
+  var checkBox = document.getElementById("source4");
+  var guestDetails = document.getElementById("externalDetails");
+  var experienceContent = document.getElementById("guestPerspectiveContent");
+  var recognitionContent = document.getElementById("recognition");
+  var followUpContent = document.getElementById("followUp");
+  if (checkBox.checked == true) {
+    guestDetails.style.display = "block";
+    experienceContent.style.display = "block";
+    recognitionContent.style.display = "block";
+    followUpContent.style.display = "block";
+  } else {
+    guestDetails.style.display = "none";
+    experienceContent.style.display = "block";
+    recognitionContent.style.display = "none";
+    followUpContent.style.display = "none";
+  }
+}
+
+function generalPraiseReveal() {
+  var checkBox = document.getElementById("target2");
+  var generalPraise = document.getElementById("whatWasGreat");
+  if (checkBox.checked == true) {
+    generalPraise.style.display = "block";
+  } else {
+    generalPraise.style.display = "none";
+  }
+}
+
+function specificPraiseReveal() {
+  var checkBox = document.getElementById("target1");
+  var specificPraise = document.getElementById("specificPraise");
+  if (checkBox.checked == true) {
+    specificPraise.style.display = "block";
+  } else {
+    specificPraise.style.display = "none";
   }
 }
 
@@ -25,10 +105,26 @@ function toggleItem() {
 
 function specificIncidentReveal() {
   var checkBox = document.getElementById("praiseType1");
-  var incidentDetails = document.getElementById("whatHappened");
+  var incidentDetails = document.getElementById("specificEmployeeWhatWasGreat");
   if (checkBox.checked == true) {
     incidentDetails.style.display = "block";
   } else {
     incidentDetails.style.display = "none";
+  }
+}
+
+function consistentGreatnessReveal() {
+  var checkBox = document.getElementById("praiseType2");
+  var greatnessDetails = document.getElementById("whatIsGreat");
+  var recognitionContent = document.getElementById("recognition");
+  var followUpContent = document.getElementById("followUp");
+  if (checkBox.checked == true) {
+    greatnessDetails.style.display = "block";
+    recognitionContent.style.display = "block";
+    followUpContent.style.display = "block";
+  } else {
+    greatnessDetails.style.display = "none";
+    recognitionContent.style.display = "none";
+    followUpContent.style.display = "none";
   }
 }
